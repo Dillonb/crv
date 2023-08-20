36b9bcb7d4c5c676cc1bc0a3662c14b4d6bb2d78
Dillon Beliveau <dillonbeliveau@gmail.com>
2023-08-20 02:53:11 +0000

    Fix test function call

--- test/test_functioncall.c
+++ test/test_functioncall.c
@@ -1,6 +1,7 @@
 #include <crv.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #ifndef WIN32
 #include <sys/mman.h>
@@ -62,18 +63,30 @@ void mprotect_rwx(uint8_t* cache, size_t size) {
 #define crv_aligned_free free
 #endif
 
+size_t align_buf_size(int buf_size, int page_size) {
+    size_t remainder = buf_size % page_size;
+    if (remainder != 0) {
+        return buf_size + (page_size - remainder);
+    }
+    return buf_size;
+}
+
 int main(int argc, char** argv) {
+#ifdef WIN32
+    int page_size = 4096;
+#else
+    int page_size = sysconf(_SC_PAGE_SIZE);
+#endif
     CRV_CTX* ctx = CRV_Init();
 
     CRV_EmitAddi(ctx, a0, a0, 1);
     CRV_EmitJalr(ctx, zero, ra, 0);
 
-    size_t buf_size = CRV_CodeSize(ctx);
-    uint8_t* buf = crv_aligned_malloc(buf_size, 4096);
-    mprotect_rwx(buf, buf_size);
+    size_t buf_size = align_buf_size(CRV_CodeSize(ctx), page_size);
+    uint8_t* buf = crv_aligned_malloc(buf_size, page_size);
+    mprotect_rwx(buf, page_size);
     CRV_Encode(ctx, buf);
     CRV_Free(ctx);
-
 
 #ifdef __riscv
     int (*add_one)(int x) = (void*)buf;
