7bc9492630b66eb173c363ae4dcf4162652a2def
Dillon Beliveau <dillonbeliveau@gmail.com>
2023-08-19 19:17:40 -0700

    Test function call

---
+++ test/test_functioncall.c
@@ -0,0 +1,85 @@
+#include <crv.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef WIN32
+#include <sys/mman.h>
+#include <errno.h>
+#include <unistd.h>
+#else
+#include <windows.h>
+#include <memoryapi.h>
+#endif
+
+void mprotect_error() {
+#ifdef WIN32
+    LPVOID lpMsgBuf;
+    DWORD error = GetLastError();
+
+    DWORD bufLen = FormatMessage(
+            FORMAT_MESSAGE_ALLOCATE_BUFFER |
+            FORMAT_MESSAGE_FROM_SYSTEM |
+            FORMAT_MESSAGE_IGNORE_INSERTS,
+            NULL,
+            error,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            (LPTSTR) &lpMsgBuf,
+            0, NULL );
+    LPCSTR lpMsgStr = (LPCSTR)lpMsgBuf;
+
+    if (bufLen) {
+        printf("VirtualProtect failed! Code: dec %lu hex %lX Message: %s\n", error, error, lpMsgStr);
+        exit(1);
+    } else {
+        printf("VirtualProtect failed! Code: %lu\n", error);
+        exit(1);
+    }
+#else
+    printf("mprotect failed! %s\n", strerror(errno));
+    exit(1);
+#endif
+}
+
+
+void mprotect_rwx(uint8_t* cache, size_t size) {
+#ifdef WIN32
+    DWORD oldProtect = 0;
+    if (!VirtualProtect(cache, size, PAGE_EXECUTE_READWRITE, &oldProtect)) {
+        mprotect_error();
+    }
+#else
+    if (mprotect(cache, size, PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
+        mprotect_error();
+    }
+#endif
+}
+
+#ifdef WIN32
+#define crv_aligned_malloc _aligned_malloc
+#define crv_aligned_free _aligned_free
+#else
+#define crv_aligned_malloc aligned_alloc
+#define crv_aligned_free free
+#endif
+
+int main(int argc, char** argv) {
+    CRV_CTX* ctx = CRV_Init();
+
+    CRV_EmitAddi(ctx, a0, a0, 1);
+    CRV_EmitJalr(ctx, zero, ra, 0);
+
+    size_t buf_size = CRV_CodeSize(ctx);
+    uint8_t* buf = crv_aligned_malloc(buf_size, 4096);
+    mprotect_rwx(buf, buf_size);
+    CRV_Encode(ctx, buf);
+    CRV_Free(ctx);
+
+
+#ifdef __riscv
+    int (*add_one)(int x) = (void*)buf;
+    printf("1 + 1 = %d\n", add_one(1));
+#else
+    printf("Not running on a RISC-V host, not executing code.\n");
+#endif
+    crv_aligned_free(buf);
+}
